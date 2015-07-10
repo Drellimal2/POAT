8c2a9e7a2a13c331d272bc71c4dd7a51df892798
Justen Morgan <justenamorgan@gmail.com>
2015-07-10 13:10:51 -0700

    Stuff

---
+++ Web/login.php
@@ -0,0 +1,42 @@
+<html ng-app="POATApp" >
+    <head>
+        <title>
+            Poat
+        </title>
+        
+        <script src="js/angular/angular.js"></script>
+        <script src="js/angular/ui-bootstrap-tpls-0.11.2.js"></script>
+        
+        <!-- Bootstrap -->
+        <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css" />
+        <script src="bootstrap/js/jquery-2.1.4.min.js"></script>
+        <script src="bootstrap/js/bootstrap.min.js"></script>
+		<script src="bootstrap/js/jquery-ui.min.js"></script>
+		<script src="bootstrap/js/jquery.min.js"></script>
+        
+        
+        
+        <!-- Custom -->
+        <link href="css/home.css" rel="stylesheet" type="text/css" />
+		<link href="images/flaticon/flaticon.css" rel="stylesheet" type="text/css" />
+        <script src="js/app.js"></script>
+        <script src='js/controllers/MainController.js'></script>
+        <script src="js/directives/poat-header.js"></script>
+        <script src="js/testnotifs.js"></script>
+        
+        
+    </head>
+	
+	<body>
+		<poatheader></poatheader>
+		
+		<div  class="col-md-12 container" >
+			
+			
+		</div>
+		
+		
+	</body>
+	
+	
+</html>
