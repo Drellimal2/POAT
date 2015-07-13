aecd16d6e9a14031fa577e8009d8218dfefae9a5
Drellimal2 <dmiller2310@hotmail.com>
2015-07-12 23:19:00 -0500

    More UI things

--- Web/js/controllers/HomeController.js
+++ Web/js/controllers/HomeController.js
@@ -1,33 +1,33 @@
 app.controller('HomeCtrl', function ($scope) {
 	
-	var what = {
-		Question : "What is Poat?",
-		Answer: "Poat is a student companion, and assignment Tracker.",
-		Special : "text"
+	var courses = {
+		Feature: "Courses",
+		Details: "View courses and assign assignments to them as well as keep your schedule.",
+		imgUrl : "http://cdn2.knowyourmobile.com/sites/knowyourmobilecom/files/styles/article_main_wide_image/public/Array/samsung_galaxy_note_4-14_0.jpg?itok=HhjWHyGL"
 	};
 	
-	var why  = {
-		Question : "Why Poat and not another ToDo?",
-		Answer: "Firstly Poat is more than another ToDo List, it's a student companion and as such has special feautures that would better suit a student than another todo list.",
-		Special : "text"
+	var dynamicpriority  = {
+		Feature: "Dynamic Priority",
+		Details: "Custom algorithm uses due date and assignment completedness to set reccommended priorities.",
+		imgUrl : "http://cdn.cultofandroid.com/wp-content/uploads/2015/03/kv-phones.png"
 	};
 	
-	var whyname = {
-		Question : "What does Poat mean?",
-		Answer: "The name of the app was originally ProjectOpus:Assignment Tracker but that was too wordy and as such we shortened it and hope that when it gets popular the name will stick.",
-		Special : "text"
+	var subtask = {
+		Feature: "Subtasks",
+		Details: "Split large assignments into subtasks and set timelines to complete each.",
+		imgUrl : "http://r3.whistleout.com.au/public/images/blog/2012/05/htc_one_x_review_11.jpg"
 		
 	};
 	
 	var features = {
-		Question: "What are some of the features?",
-		Answer: "Alot",
-		Special : "text"
+		Feature: "More states for items ...",
+		Details: "Complete, Started, Submitted ... Always know where you stand on a project.",
+		imgUrl : "http://i-cdn.phonearena.com/images/reviews/110579-image/HTC-One-X-vs-Apple-iPhone-4S-06.jpg"
 		
 		
 	}
 	
-	$scope.info = [ what, why, whyname, features];
+	$scope.info = [ dynamicpriority, subtask, courses, features];
 	
 	
 	
