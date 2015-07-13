659371d441d9d07e73e45ae1f730c7266687a0f9
Justen Morgan <justenamorgan@gmail.com>
2015-07-13 16:10:14 -0700

    Update

--- Web/js/controllers/HomeController.js
+++ Web/js/controllers/HomeController.js
@@ -4,15 +4,15 @@ app.controller('HomeCtrl', function ($scope) {
 		Feature: "Courses",
 		Details: "View courses and assign assignments to them as well as keep your schedule.",
 
-		imgUrl : "/images/POAT_blue_noback.png"
-	};
+		imgUrl : "images/POAT_blue_noback.png"
 	//imgUrl : "http://cdn2.knowyourmobile.com/sites/knowyourmobilecom/files/styles/article_main_wide_image/public/Array/samsung_galaxy_note_4-14_0.jpg?itok=HhjWHyGL"
+	};
 	
 	var dynamicpriority  = {
 		Feature: "Dynamic Priority",
 		Details: "Custom algorithm uses due date and assignment completedness to set reccommended priorities.",
 		
-		imgUrl : "/images/POAT_blue_noback.png"
+		imgUrl : "images/POAT_blue_noback.png"
 	};
 	//imgUrl : "http://cdn.cultofandroid.com/wp-content/uploads/2015/03/kv-phones.png"
 	
@@ -20,7 +20,7 @@ app.controller('HomeCtrl', function ($scope) {
 		Feature: "Subtasks",
 		Details: "Split large assignments into subtasks and set timelines to complete each.",
 		
-		imgUrl : "/images/POAT_blue_noback.png"
+		imgUrl : "images/POAT_blue_noback.png"
 		
 	};
 	//imgUrl : "http://r3.whistleout.com.au/public/images/blog/2012/05/htc_one_x_review_11.jpg"
@@ -29,7 +29,7 @@ app.controller('HomeCtrl', function ($scope) {
 		Feature: "More states for items ...",
 		Details: "Complete, Started, Submitted ... Always know where you stand on a project.",
 		
-		imgUrl : "/images/POAT_blue_noback.png"
+		imgUrl : "images/POAT_blue_noback.png"
 	}
 	//imgUrl : "http://i-cdn.phonearena.com/images/reviews/110579-image/HTC-One-X-vs-Apple-iPhone-4S-06.jpg"
 	
