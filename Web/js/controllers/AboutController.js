339c9d63374ff392608efe6e73b05257c393ea37
Drellimal2 <dmiller2310@hotmail.com>
2015-07-15 11:43:42 -0500

    about us

--- Web/js/controllers/AboutController.js
+++ Web/js/controllers/AboutController.js
@@ -9,23 +9,23 @@ app.controller("AboutCtrl", function ($scope) {
 		alias: ["Drellimal", "Drell1mal"],
 		bio: "Dane is the 'Founder' of BlueScreen1 if it ever had any. He brought the group together and had the idea for this project. He is also the lead Android Developer on Poat but also worked on the Web Version. His favourite (yes we're using Brittish English) programming language is python and his hobbies include coding, origami and gaming. Frequent saying \"Awks Awks Awks\"",
 		links: [],
-		imgUrl: "https://scontent-mia1-1.xx.fbcdn.net/hphotos-xta1/v/t1.0-9/q83/p206x206/10641082_10204832189462230_5488347243277759855_n.jpg?oh=0d5e5b06101d15f10051d5333c737def&oe=5614E789",
-		colour: "#57D3FF"
+		imgUrl: "images/dane.png",
+		colour: "#1793BD"
 		
 	},	{
 		name: "Justen Morgan",
 		alias: ["Boshes", "Boshy", "J10"],
 		bio: "Justen (Boshes) is the comic relief if you need one. Justen with his hilarious \"GG we failed\" phrase after every roadbump relieved some stress in the development process. He has an impressive 5,500 song music library and an even more impressive shirt collection which leaves everyone in envy and wondering where he got them. Notable quote \"I dont need gym I'm already sexy.\"",
 		links: [],
-		imgUrl: "https://scontent-mia1-1.xx.fbcdn.net/hphotos-xap1/v/t1.0-9/p206x206/1505576_10203772277089436_7131165601516638659_n.jpg?oh=6d2bebb868f26e8f2921c06dfdfb22e7&oe=562536D1",
-		colour: "#00F663"
+		imgUrl: "images/justen.png",
+		colour: "#00B623"
 	},	{
 	
 		name: "Travis Smith",
 		alias: ["TevyT"],
 		bio: "TevyT the computer guy. Though we are all developers and all had similar grades, TevyT was still the one any and every1 went to for computer/programming related questions. Using straight linux he is dedicated to programming and was the first to start prepare for the future with Ruby. Plus he has a car.",
 		links: [],
-		imgUrl: "https://scontent-mia1-1.xx.fbcdn.net/hphotos-xfp1/v/t1.0-9/282076_10150273243398620_5074268_n.jpg?oh=b092080e421bce1140c1ebb1720c14ca&oe=56117F57",
+		imgUrl: "images/travis.png",
 		colour: "#FF3A43"
 		
 	}];
